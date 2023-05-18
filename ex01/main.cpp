6b1c0d307a9393de4a2e95c367041d5e86c0f458
Mathis CATAL <catalmathis@gmail.com>
2023-05-19 01:08:20 +0200

    verif

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mcatal-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 04:04:02 by mcatal-d          #+#    #+#             */
-/*   Updated: 2023/05/18 18:22:33 by mcatal-d         ###   ########.fr       */
+/*   Updated: 2023/05/18 18:26:20 by mcatal-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@
 int main()
 {
     ScavTrap    scav("Bot");
-    // ClapTrap    clap("Mario");
+    ClapTrap    clap("Mario");
     
-    // ScavTrap    scav2(scav);
-    // ClapTrap    clap2(clap);
+    ScavTrap    scav2(scav);
+    ClapTrap    clap2(clap);
 
-    // scav.attack("Mario");
-    // clap.takeDamage(20);
-    // clap.beRepaired(10);
-    // scav.guardGate();
+    scav.attack("Mario");
+    clap.takeDamage(20);
+    clap.beRepaired(10);
+    scav.guardGate();
 
-    // for (int i = 0; i < 11; i++)
-    //     clap.beRepaired(20);
+    for (int i = 0; i < 11; i++)
+        clap.beRepaired(20);
     return (0);
 }
